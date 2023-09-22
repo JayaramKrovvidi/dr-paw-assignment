42c925e3e72f2d7b29c6ebda11e1fbbfc7aa2e29
Jayaram Krovvidi <jayaram.krovvidi@outlook.com>
2023-09-22 10:11:01 -0400

    Modified List View Component

--- components/vet-list-detail/VetListDetail.jsx
+++ components/vet-list-detail/VetListDetail.jsx
@@ -1,14 +1,51 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, TouchableOpacity, Linking } from 'react-native';
+import { Card } from '@rneui/themed';
+import { Rating } from 'react-native-ratings';
+
+import styles from './vetlistdetail.style';
+import { COLORS } from '../../constants/theme';
 
 const VetListDetail = ({ clinic }) => {
+
     return (
-        <View>
-            <Text>Name: {clinic.name}</Text>
-            <Text>Address: {clinic.address}</Text>
-            <Text>Rating: {clinic.rating.$numberDouble}</Text>
-            <Text>Total Ratings: {clinic.user_ratings_total.$numberInt}</Text>
-        </View>
+        <Card containerStyle={{ marginBottom: 16 }}>
+            <View style={{ flexDirection: 'row' }}>
+                <View style={{ width: 100, height: 100, backgroundColor: COLORS.gray2, marginRight: 16 }} />
+                <View style={{ flexDirection: 'column' }}>
+                    <Text style={{ fontSize: 18, fontWeight: 'bold', flexWrap: 'wrap', maxWidth: '80%' }}>
+                        {clinic.name}
+                    </Text>
+                    <Text style={{ flexWrap: 'wrap', maxWidth: '80%' }}>
+                        {clinic.address}
+                    </Text>
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                        <Rating
+                            imageSize={30}
+                            readonly={true}
+                            onFinishRating={parseFloat(clinic.rating.$numberDouble)}
+                            style={styles.starRatingContainer}
+                        />
+                        <Text>({clinic.user_ratings_total.$numberInt} Ratings)</Text>
+                    </View>
+                </View>
+            </View>
+            <View style={{ marginTop: 16, flexDirection: 'row', justifyContent: 'space-between' }}>
+                <View style={{ flexDirection: 'column' }}>
+                    <TouchableOpacity onPress={() => alert('Services Provided')}>
+                        <Text style={{ color: COLORS.secondary }}>Services Provided</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => alert('Book an Appointment')}>
+                        <Text style={{ color: COLORS.secondary }}>Book an Appointment</Text>
+                    </TouchableOpacity>
+                </View>
+                <TouchableOpacity onPress={() => Linking.openURL(`https://maps.google.com/?q=${clinic.name} ${clinic.address}`)} style={{ alignSelf: 'flex-end' }}>
+                    <View style={{ backgroundColor: COLORS.secondary, padding: 10, borderRadius: 5 }}>
+                        <Text style={{ color: COLORS.white }}>Maps</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
+        </Card>
     );
 };
 
---
+++ components/vet-list-detail/vetlistdetail.style.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+
+const styles = StyleSheet.create({
+    starRatingContainer: {
+        shadowColor: 'rgba(0,0,0, .2)',
+        shadowOffset: { height: 0, width: 0 },
+        shadowOpacity: 0,
+        shadowRadius: 0,
+        marginRight: '16px'
+    }
+});
+
+export default styles;
